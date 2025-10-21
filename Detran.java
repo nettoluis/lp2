ac931ce6abf96cc782f0a212d93b7e310891b571
Luis Netto <121206720+nettoluis@users.noreply.github.com>
2025-10-21 09:49:15 -0300

    Dia 01

---
+++ Detran.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Detran {
+   /**
+    * Laboratório de Programação 2 - Lab 1
+    * 
+    * @author Luis Netto - 20250019231
+    */
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        int diasLicenciamento = leitor.nextInt();
+        int diasCarteira = leitor.nextInt();
+        double bafometro = leitor.nextDouble();
+
+        String deveSerApreendido = diasLicenciamento >= 30 | diasCarteira >= 30 | bafometro > 0.05 ? "True" : "False";
+
+        System.out.println(deveSerApreendido);
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+   /**
+    * Laboratório de Programação 2 - Lab 1
+    * 
+    * @author Luis Netto - 20250019231
+    */
+    public static void main(String[] args) {
+        System.out.printf("Hello Dirlididi!");
+    }
+}
