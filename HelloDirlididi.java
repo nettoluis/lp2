51a08820250c0514eaa89d398e8d45c31856abab
Luis Netto <luisnetto.2013@gmail.com>
2025-10-21 13:41:06 -0300

    Renomeando os programas para o compatível com o Dirlididi

---
+++ HelloDirlididi.java
@@ -0,0 +1,10 @@
+public class HelloDirlididi {
+   /**
+    * Laboratório de Programação 2 - Lab 1
+    * 
+    * @author Luis Netto - 20250019231
+    */
+    public static void main(String[] args) {
+        System.out.printf("Hello Dirlididi!");
+    }
+}
