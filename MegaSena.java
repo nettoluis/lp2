9b4f237023617b96a79150f49ed53086fd4973c8
nettoluis <luis.goncalves.da.rocha.netto@lcc1-17.lcc.ufcg.edu.br>
2025-10-30 10:36:06 -0300

    Abunitezando

--- MegaSena.java
+++ MegaSena.java
@@ -6,35 +6,34 @@ public class MegaSena {
     * 
     * @author Luis Netto - 20250019231
     */
+    public static boolean estaContido(int elemento, int[] sequencia) {
+        for (int e : sequencia) {
+            if (e == elemento) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
     public static void main(String[] args) {
         try (Scanner leitor = new Scanner(System.in)) {
             int entrada = leitor.nextInt();
 
             for (int i = 0; i < entrada; ++i) {
                 for (int j = 0; j < 6; ++j) {
-                    int numero1 = (int) ((Math.random() * 100) % 60) + 1, numero2, numero3, numero4, numero5, numero6;
+                    int[] numeros = new int[6];
 
+                    for (int k = 0; k < 6; ++k) {
+                        int numero = 0;
                         do {
-                             numero2 = (int) ((Math.random() * 100) % 60) + 1;
-                        } while (numero2 == numero1); 
-                        
-                        do {
-                             numero3 = (int) ((Math.random() * 100) % 60) + 1;
-                        } while (numero3 == numero2); 
-                        
-                        do {
-                             numero4 = (int) ((Math.random() * 100) % 60) + 1;
-                        } while (numero4 == numero3); 
+                            numero = (int) (Math.random() * 100) % 60 + 1;
+                        } while (estaContido(numero, numeros));
 
-                        do {
-                             numero5 = (int) ((Math.random() * 100) % 60) + 1;
-                        } while (numero5 == numero4); 
-                        
-                        do {
-                             numero6 = (int) ((Math.random() * 100) % 60) + 1;
-                        } while (numero6 == numero5); 
-
-                    System.out.printf("%02d %02d %02d %02d %02d %02d\n", numero1, numero2, numero3, numero4, numero5, numero6);
+                        numeros[k] = numero;
+                    }
+                    System.out.printf("%02d %02d %02d %02d %02d %02d\n", numeros[0], numeros[1], numeros[2], numeros[3], numeros[4], numeros[5]);
                 }
             }
         }
